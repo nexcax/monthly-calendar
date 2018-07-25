4d7af6ed576432e7b2dce8d72c8905b3bdca1cc2
Xavier Castillo <nex.cax@gmail.com>
2018-07-25 13:22:46 -0500

    Add support for weekends and coloring conditions

--- angular-version/src/app/calendar/calendar.component.ts
+++ angular-version/src/app/calendar/calendar.component.ts
@@ -34,28 +34,57 @@ export class CalendarComponent implements AfterViewInit {
     const end = date.clone().add(entryData.daysNumber, 'days');
     let incrementDay = 0;
     const start = date.clone();
+    const formatKey = 'MMMM YYYY';
 
     // calculate all days for the calendars
-    while (current.toDate().getTime() <= end.toDate().getTime()) {
-      if (!(current.format('YYYY-MMMM') in calendarData)) {
-        calendarData[current.format('YYYY-MMMM')] = [];
-        for (let i = 0; i < current.day(); i++) {
-          calendarData[current.format('YYYY-MMMM')].push({
+    let lastMonth = current.clone();
+    let lastPrintedDate = current.clone();
+    while (current.toDate().getTime() < end.toDate().getTime()) {
+      if (lastMonth.format(formatKey) !== current.format(formatKey)) {
+        // complete the last disabled days in the month
+        for (let i = lastMonth.clone().endOf('month').day(); i < 6; i++) {
+          const lastWeekDay = (calendarData[lastMonth.format(formatKey)][calendarData[lastMonth.format(formatKey)].length - 1].weekDay);
+          calendarData[lastMonth.format(formatKey)].push({
             disabled: true,
             dayNumber: '',
-            month: current.clone().add(i, 'days').format('M'),
-            isWeekend: [0, 6].indexOf(current.clone().add(i, 'days').day()) >= 0 ? true : false
+            isHoliday: false,
+            weekDay: lastWeekDay + 1
+          });
+        }
+        lastMonth = current.clone();
+      }
+      if (!(current.format(formatKey) in calendarData)) {
+        calendarData[current.format(formatKey)] = [];
+        for (let i = 0; i < current.day(); i++) {
+          calendarData[current.format(formatKey)].push({
+            disabled: true,
+            dayNumber: '',
+            isHoliday: false,
+            weekDay: current.clone().day() - current.clone().subtract(i, 'days').day()
           });
         }
       }
-      calendarData[current.format('YYYY-MMMM')].push({
+      calendarData[current.format(formatKey)].push({
         disabled: false,
         dayNumber: current.format('D'),
-        month: current.format('M'),
-        isWeekend: [0, 6].indexOf(current.day()) >= 0 ? true : false
+        isHoliday: false,
+        weekDay: current.day()
       });
+      lastPrintedDate = current;
       incrementDay++;
       current = start.clone().add(incrementDay, 'days');
+    }
+    if (lastPrintedDate.day() < 6) {
+      for (let i = lastPrintedDate.day(); i < 6; i++) {
+        const lastWeekDay =
+          calendarData[lastPrintedDate.format(formatKey)][calendarData[lastPrintedDate.format(formatKey)].length - 1].weekDay;
+        calendarData[lastPrintedDate.format(formatKey)].push({
+          disabled: true,
+          dayNumber: '',
+          isHoliday: false,
+          weekDay: lastWeekDay + 1
+        });
+      }
     }
 
     this.calendars = Object.keys(calendarData);
